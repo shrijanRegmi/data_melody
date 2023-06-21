33b78675ddb32ac6e29aa5bfd63fbf0f8316f5ba
Shrijan Regmi <your.email@example.com>
2023-06-21 17:59:01 +0545

    updated UI

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:data_melody/data_melody.dart';
 import 'package:data_melody/enums/data_melody_player_type.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +22,7 @@ class _MyAppState extends State<MyApp> {
   late Stream<Map<String, dynamic>> _receivedDataStream;
   final _messageController = TextEditingController(text: 'hello world');
   var _selectedPlayerType = DataMelodyPlayer.ultrasonicNormal;
+  var _volume = 50;
 
   @override
   void initState() {
@@ -40,22 +42,32 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _inputBuilder(),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                _actionsBuilder(),
-                const SizedBox(
-                  height: 50.0,
-                ),
-                _receivedDataBuilder(),
-              ],
+        body: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _inputBuilder().pX(20),
+                  const SizedBox(
+                    height: 50.0,
+                  ),
+                  _volumeSlider(),
+                  const SizedBox(
+                    height: 50.0,
+                  ),
+                  _playerSelector().pX(20),
+                  const SizedBox(
+                    height: 50.0,
+                  ),
+                  _buttonBuilder().pX(20),
+                  const SizedBox(
+                    height: 50.0,
+                  ),
+                  _receivedDataBuilder().pX(20),
+                ],
+              ),
             ),
           ),
         ),
@@ -64,45 +76,90 @@ class _MyAppState extends State<MyApp> {
   }
 
   Widget _inputBuilder() {
-    return TextFormField(
-      controller: _messageController,
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Message',
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Message:'),
+        const SizedBox(
+          height: 15.0,
+        ),
+        TextFormField(
+          controller: _messageController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Message',
+          ),
+        ),
+      ],
     );
   }
 
-  Widget _actionsBuilder() {
+  Widget _volumeSlider() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Volume:').pX(20),
+        SfSlider(
+          min: 0,
+          max: 100,
+          value: _volume,
+          interval: 10,
+          showTicks: true,
+          showLabels: true,
+          enableTooltip: true,
+          minorTicksPerInterval: 1,
+          onChanged: (value) {
+            setState(() {
+              if (value is double) {
+                _volume = value.toInt();
+              } else if (value is int) {
+                _volume = value;
+              }
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _playerSelector() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Player Type:'),
+        const SizedBox(
+          height: 10.0,
+        ),
+        DropdownButton<DataMelodyPlayer>(
+          value: _selectedPlayerType,
+          iconEnabledColor: Colors.blue,
+          isExpanded: true,
+          items: DataMelodyPlayer.values.map((e) {
+            return DropdownMenuItem<DataMelodyPlayer>(
+              value: e,
+              child: Text(
+                e.name,
+                style: TextStyle(
+                  color: _selectedPlayerType == e ? Colors.blue : null,
+                ),
+              ),
+            );
+          }).toList(),
+          onChanged: (val) {
+            if (val != null) {
+              setState(() {
+                _selectedPlayerType = val;
+              });
+            }
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buttonBuilder() {
     return Row(
       children: [
-        Expanded(
-          child: DropdownButton<DataMelodyPlayer>(
-            value: _selectedPlayerType,
-            iconEnabledColor: Colors.blue,
-            items: DataMelodyPlayer.values.map((e) {
-              return DropdownMenuItem<DataMelodyPlayer>(
-                value: e,
-                child: Text(
-                  e.name,
-                  style: TextStyle(
-                    color: _selectedPlayerType == e ? Colors.blue : null,
-                  ),
-                ),
-              );
-            }).toList(),
-            onChanged: (val) {
-              if (val != null) {
-                setState(() {
-                  _selectedPlayerType = val;
-                });
-              }
-            },
-          ),
-        ),
-        const SizedBox(
-          width: 20.0,
-        ),
         Expanded(
           child: ElevatedButton(
             child: const Text('Start Playing Sound'),
@@ -110,6 +167,7 @@ class _MyAppState extends State<MyApp> {
               await _dataMelody.startSendingData(
                 data: _messageController.text,
                 player: _selectedPlayerType,
+                volume: _volume,
               );
             },
           ),
@@ -119,16 +177,51 @@ class _MyAppState extends State<MyApp> {
   }
 
   Widget _receivedDataBuilder() {
-    return StreamBuilder<Map<String, dynamic>>(
-      stream: _receivedDataStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final data = snapshot.data!;
-          return Text('Received data: $data');
-        }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Received Data:'),
+        const SizedBox(
+          height: 20.0,
+        ),
+        Center(
+          child: StreamBuilder<Map<String, dynamic>>(
+            stream: _receivedDataStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final snapData = snapshot.data!;
+                final data = snapData['data'];
 
-        return const Text('Nothing Received');
-      },
+                if (data != null) {
+                  return Text(
+                    '$data',
+                    style: const TextStyle(
+                      color: Colors.green,
+                    ),
+                  );
+                }
+              }
+
+              return const Text(
+                'Nothing received yet',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontStyle: FontStyle.italic,
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+extension on Widget {
+  Widget pX(final double x) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: x),
+      child: this,
     );
   }
 }
